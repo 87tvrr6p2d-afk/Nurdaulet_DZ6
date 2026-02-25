f566d2a21beddb599dc7cb83958306ab1895c167
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-02-25 12:39:08 +0500

    Create Context.java

---
+++ Context.java
@@ -0,0 +1,15 @@
+public class TravelBookingContext {
+
+    private CostCalculationStrategy strategy;
+
+    public void setStrategy(CostCalculationStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public double calculate(TravelRequest request) {
+        if (strategy == null) {
+            throw new IllegalStateException("Strategy not selected!");
+        }
+        return strategy.calculateCost(request);
+    }
+}
