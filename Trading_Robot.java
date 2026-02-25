3c3597155fbd143f7d619f1afebb31578c15ed95
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-02-25 12:48:31 +0500

    Update Trading_Robot.java

--- Trading_Robot.java
+++ Trading_Robot.java
@@ -1,1 +1,17 @@
+public class TradingRobot implements Observer {
 
+    private double threshold;
+
+    public TradingRobot(double threshold) {
+        this.threshold = threshold;
+    }
+
+    @Override
+    public void update(String stockName, double price) {
+        if (price > threshold) {
+            System.out.println("Robot: Selling " + stockName);
+        } else {
+            System.out.println("Robot: Buying " + stockName);
+        }
+    }
+}
