9adaf39d70520bbfe780b73c2065683d4a5453f3
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-02-25 12:47:09 +0500

    Create Trader.java

---
+++ Trader.java
@@ -0,0 +1,15 @@
+public class Trader implements Observer {
+
+    private String name;
+
+    public Trader(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String stockName, double price) {
+        System.out.println("Trader " + name +
+                " notified: " + stockName +
+                " new price: " + price);
+    }
+}
